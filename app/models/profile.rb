1af513acb399e92516127059d797924f598eac8d
Jan Kalčík <jan.kalcik@joshik.net>
2012-02-20 13:34:11 +0100

    + zmena validace z on_update na validaci pri kazde akci

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,8 +3,8 @@ class Profile < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :first_name, :presence => :true, :on => :update
-  validates :second_name, :presence => :true, :on => :update
-  validates :email, :presence => :true, :on => :update
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :update
+  validates :first_name, :presence => :true
+  validates :second_name, :presence => :true
+  validates :email, :presence => :true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
