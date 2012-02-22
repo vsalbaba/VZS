57f8ad54441af928030c921fd5e6be30a9dcb900
Jan Kalčík <jan.kalcik@joshik.net>
2012-02-22 09:39:08 +0100

    oprava rspec testu

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 
 describe Profile do
-  describe 'validate_age' do
-    it 'validate age' do
-      profile = Factory :profile
-      profile.user_age.should eq 10
-      profile = Factory :profile, :birthdate => 12.years.ago
-      profile.user_age.should_not eq 10
-    end
+  before :each do 
+    @profile = Factory :profile 
+  end
+
+  it 'should be valid when age equal to 10' do
+    @profile.birthdate = 10.years.ago
+    @profile.user_age.should eq 10
+  end
+
+  it 'should be valid when age is not equal 10' do
+    @profile.birthdate = 12.years.ago
+    @profile.user_age.should_not eq 10
+  end
+
+  it 'should be valid when user_age is false' do
+    @profile.birthdate = nil
+    @profile.user_age.should eq false
   end
 end
