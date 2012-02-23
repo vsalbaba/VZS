bdc01ce2ca5270c14c77d086ca7baa111d58192d
Vojtech Salbaba <darktatka@gmail.com>
2012-02-23 14:51:03 +0100

    testy

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -1,15 +1,7 @@
 FactoryGirl.define do
   factory :profile do
-    user_id 1001
+    user 
     first_name 'rspec'
-    second_name '_user'
-    email 'test@test.cz'
-    street 'rs'
-    city 'pec'
-    telephone '888888888'
-    postal_code '11102'
-    house_number '1'
-    birthdate 10.years.ago
-    birthnumber '102816219'
+    second_name 'user'
   end
 end
