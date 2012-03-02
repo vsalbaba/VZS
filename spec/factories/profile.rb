ead5d6574411a4c2da7cb2e06546491e58f6dc42
Queria Sa-Tas <queria@sa-tas.net>
2012-03-02 15:59:43 +0100

    factory na profil uzivatele nyni obsahuje vsechny potrebne atributy

--- spec/factories/profile.rb
+++ spec/factories/profile.rb
@@ -3,5 +3,9 @@ FactoryGirl.define do
     user 
     first_name 'rspec'
     second_name 'user'
+    sequence(:email) { |n| "user_${n}@example.org" }
+    telephone '123456789'
+    birthdate '2011-01-01'
+    birthnumber '110101/2222'
   end
 end
