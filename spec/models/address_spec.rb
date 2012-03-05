b5c20fd9b317f29fa23db55cb9f1dc6f3943b5d5
Jan Kalčík <jan.kalcik@joshik.net>
2012-03-05 13:46:32 +0100

    pridan test pro model address

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,7 +1,37 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Address do
-  it "should be valid" do
-    Address.new.should be_valid
+
+  before :each do
+    @address = Factory :address
+  end
+
+  describe 'validations' do
+    it 'should not be valid without street' do
+      @address.street = nil
+      @address.should_not be_valid
+      @address.errors[:street].should_not be_blank
+    end
+
+    it 'should not be valid without house_number' do
+      @address.house_number = nil
+      @address.should_not be_valid
+      @address.errors[:house_number].should_not be_blank
+    end
+
+    it 'should not be valid without city' do
+      @address.city = nil
+      @address.should_not be_valid
+      @address.errors[:city].should_not be_blank
+    end
+
+    it 'should not be valid without postcode' do
+      @address.postcode = nil
+      @address.should_not be_valid
+      @address.errors[:postcode].should_not be_blank
+    end
   end
 end
+  
+
+
