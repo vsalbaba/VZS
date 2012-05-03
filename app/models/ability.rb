87c625ca35de26b811047fe5243bba1a45a43c68
Queria Sa-Tas <queria@sa-tas.net>
2012-05-03 12:54:05 +0200

    rozsirujici vlastnosti profilu vidi clenove/vybor

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,6 +14,13 @@ class Ability
       return
     end
 
+    article_rules(user)
+    comments_rules(user)
+    user_rules(user)
+
+  end
+
+  def article_rules(user)
     if user.group == BOARD
       can :read, Article, :group => [OUTSIDER, MEMBER, BOARD]
       can :create, Article, :group => [nil, OUTSIDER, MEMBER, BOARD], :user_id => user.id
@@ -27,17 +34,28 @@ class Ability
     else
       can :read, Article, :group => OUTSIDER # neprihlaseni smi cist clanky pro necleny
     end
+  end
 
+  def comments_rules(user)
     # komentare
     can :read, Comment
     if [BOARD,MEMBER,OUTSIDER].include? user.group
       # vsichni registrovani smi psat komentare
       can :create, Comment, :article => { :commentable => true }
     end
+  end
 
-    # pouze pro cleny
-    if user.group and user.group >= MEMBER
-      can :read_members, Profile
+  def user_rules(user)
+    if user.group >= MEMBER
+      # clenove smi ...
+      can :read_members, Profile # ... prohlizet seznam clenu
+      can :manage, Profile, :user_id => user.id # ... upravovat svuj profil
+    end
+
+    if user.group >= BOARD
+      # clenove vyboru a vyssi smi spravovat uzivatele a rozsirujici atributy
+      # profilu (RC, Adresa, Skupina apod)
+      can :manage, User
     end
   end
 end
