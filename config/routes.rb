48b7353ca35deb72f0d61aec1ba0504cf0234fc3
Queria Sa-Tas <queria@sa-tas.net>
2012-05-03 17:22:18 +0200

    odstraneny nepotrebne akce a sablony Adres

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Vzs::Application.routes.draw do
     resources :comments
   end
 
-  resources :addresses
-  #resources :profiles # nested_forms => @users
   resources :articles
 
   get "user_sessions/new"
