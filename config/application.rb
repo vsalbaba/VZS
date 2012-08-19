5cf80c58330573e02ee2a3d1d0eb3af8175ffb5d
Vojtech Salbaba <darktatka@gmail.com>
2012-08-19 11:01:27 +0200

    nacitani prekladu i z podslozek config

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,7 @@ module Vzs
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[File.join(Rails.root.join, 'config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.default_locale = :cs
 
     # Configure the default encoding used in templates for Ruby 1.9.
