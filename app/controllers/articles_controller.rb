86c37543295460db72ec413b022e28e620472915
Vojtěch Šalbaba <vojtech.salbaba@friendlysystems.cz>
2012-09-20 10:10:47 +0200

    Razeni clanku ve vypisu
    [Finishes #35482393]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   load_and_authorize_resource
 
   def index
-    @articles = @articles.order('sticky DESC').paginate(:page => params[:page])
+    @articles = @articles.order('sticky DESC, created_at DESC').paginate(:page => params[:page])
   end
 
   def show
