4ec944ad8791bfa1c4ea105d7317100291732c73
Vojtech Salbaba <darktatka@darktatka-EP45-DS4.(none)>
2012-12-06 23:36:17 +0100

    Pridani data k udalosti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206204224) do
+ActiveRecord::Schema.define(:version => 20121206214149) do
 
   create_table "addresses", :force => true do |t|
     t.string   "street"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20121206204224) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.date     "date"
   end
 
   create_table "galleries", :force => true do |t|
